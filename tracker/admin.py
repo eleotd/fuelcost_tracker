22ce526d562c4f227670071516eed47d432fbebf
Михаил Будущев <mbudushhev@bk.ru>
2026-01-10 12:39:41 +0500

    Регистрация моделей в админке

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Car, Refueling, ExpenseCategory
 
-# Register your models here.
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('brand', 'model', 'year', 'fuel_type', 'user')
+    list_filter = ('fuel_type', 'year')
+
+@admin.register(Refueling)
+class RefuelingAdmin(admin.ModelAdmin):
+    list_display = ('car', 'date', 'liters', 'price_per_liter', 'total_cost')
+    list_filter = ('date', 'car')
+
+@admin.register(ExpenseCategory)
+class ExpenseCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')

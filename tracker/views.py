aae1f3b8aab7841f643b43695d467267d3d53902
Михаил Будущев <mbudushhev@bk.ru>
2026-01-10 13:11:22 +0500

    Реализация views

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import RegisterForm
 
-# Create your views here.
+def home(request):
+    return render(request, 'tracker/home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Регистрация успешна! Теперь вы можете войти.')
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    return render(request, 'tracker/register.html', {'form': form})
+
+@login_required
+def car_list(request):
+    return render(request, 'tracker/car_list.html')
